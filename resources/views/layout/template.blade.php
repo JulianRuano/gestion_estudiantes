542845a1e358de5e04460c31041d2b2f0d9b6a03
J-Ayerbe <wesker0238@protonmail.com>
2023-10-01 03:00:56 -0500

    feat: Add loading spinner and form validation to create alumno page
    - Added a loading spinner to provide visual feedback during form submission
    - Implemented form validation for matricula, nombre, email, fecha, telefono fields
    - Displayed error messages for invalid input in the form

--- resources/views/layout/template.blade.php
+++ resources/views/layout/template.blade.php
@@ -1,18 +1,41 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
-    <script src="https://cdn.tailwindcss.com?plugins=forms,typography,aspect-ratio,line-clamp"></script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <script src="https://cdn.tailwindcss.com"></script>
+    <script src="https://kit.fontawesome.com/a9354fe65c.js" crossorigin="anonymous"></script>
     <title>@yield('title')</title>
+    <style>
+        /* Spinner */
+        .border-gray-900 {
+            border-color: #cbd5e0;
+        }
+    </style>
 </head>
+
 <body>
 
     @yield('content')
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
-    
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous">
+    </script>
+    <script>
+        const img = new Image();
+        img.src = 'https://res.cloudinary.com/dzxhdnqm4/image/upload/v1696144096/pexels-keira-burton-6147150_dg3x1z.avif';
+        img.onload = function() {
+            const spinner = document.getElementById('spinner');
+            const formulario = document.getElementById('formulario');
+            spinner.style.display = 'none';
+            formulario.style.display = "block";
+        };
+    </script>
+
 </body>
+
 </html>
